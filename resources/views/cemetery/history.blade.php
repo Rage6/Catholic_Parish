9b6d821beb622ef97ac79de001244df712ddcc58
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2022-12-21 12:01:23 -0500

    with cemetery history

--- resources/views/cemetery/history.blade.php
+++ resources/views/cemetery/history.blade.php
@@ -7,5 +7,22 @@
       <div class="sectionTitle">
         Cemetery History
       </div>
+      <div>
+        <p>
+          The history of our cemetery began as early as the 1830’s when much of the land was covered in dense forest and swamps. Before property was acquired for the purpose of a church, mass was celebrated in the log cabins of local families. The first record of mass was on the Finnegan property on Settlement East Road. This property served as a burial ground for several parishioners, such as Nicholas Bieglin, buried in 1835 and a Mrs. Fisher from around the same time period.
+        </p>
+        <p>
+          In 1834, land was purchased by an agent for the Roman Catholic Church in Richland County, Ohio for “the exclusive use and benefit of a church”. A log cabin church was completed in 1837 at the location of the stone cross in our current cemetery. It is this year, 1837, in which we see the earliest headstones. The log cabin Church was used for 16 years until construction on a new brick church was completed in 1853 to the south of the present-day cemetery.
+        </p>
+        <p>
+          In 1882, the large, stone cross in the cemetery was erected where the original log cabin church is thought to have been. This stone cross marks the gravesites reserved for priests. Father Widmer, Father Abair and Father Sutter are buried there. As of 2022, two priest-sons of the parish have graves reserved there.
+        </p>
+        <p>
+          During the early 1900’s, an epidemic spread throughout our area and took the lives of many. This caused the burial of some of the victims in a mass grave in the northwest corner of our cemetery, sometimes known as Potters Field. There are no plots available around this area because it is unknown exactly where the remains are buried, and we do not want to exhume any graves.
+        </p>
+        <p>
+          At first it seems that there is not much to the history of Sacred Heart Church or its cemetery, but, after some research, you can soon find many people are buried in our cemetery that had a large influence on the surrounding area. It is our hope that everyone will respect and appreciate those whose final resting place is Sacred Heart Cemetery.
+        </p>
+      </div>
     </div>
   @endsection
