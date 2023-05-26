24a622ab29db1ac650323abf4a74eff598124bdd
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2023-05-26 00:36:50 -0400

    shows who purchased empty plots

--- resources/views/administrator/update_deceased_list.blade.php
+++ resources/views/administrator/update_deceased_list.blade.php
@@ -4,8 +4,19 @@
   @foreach ($all_deceased as $one_deceased)
 
       <a href="{{ route('cemetery.updateform', ['id' => $one_deceased->id, 'type' => $type]) }}">
-        <button class="btn btn-primary col-12">
-          @if ($one_deceased->title) {{ $one_deceased->title }} @endif {{ $one_deceased->first_name }} {{ $one_deceased->last_name }} @if ($one_deceased->suffix_name) {{ $one_deceased->suffix_name }} @endif
+        <button class="btn btn-primary col-12" style="margin-bottom:10px">
+          @if ($one_deceased->title)
+            {{ $one_deceased->title }}
+          @endif
+          {{ $one_deceased->first_name }} {{ $one_deceased->last_name }}
+          @if ($one_deceased->suffix_name)
+            {{ $one_deceased->suffix_name }}
+          @endif
+          @if ($one_deceased->first_name === "EMPTY" && $one_deceased->last_name === "PLOT" && $one_deceased->purchased_by)
+            <div style="font-size: 0.7rem">
+              <u>Purchased By</u>: {{ $one_deceased->purchased_by }}
+            </div>
+          @endif
         </button>
       </a>
 
